298aa591c9193c7dc2d48e1854cf3b742da6d5a4
Vycktor Stark <huho387@gmail.com>
2020-06-06 14:50:28 -0400

    removing unnecessary files

--- config.py
+++
@@ -1,9 +0,0 @@
-import telepot
-from config import *
-TokenBot = "token"
-Chatsuporte = "-1001097459691"
-api = telepot.Bot(TokenBot)
-bot = api.getMe()
-bot_name = bot['first_name']
-bot_username = '@' + bot['username']
-bot_id = bot['id']
